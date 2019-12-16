01fa8f57a402eda8cbbc1bb7f68f9818e9fce541
Ihor Pedan <ihorpedan@gmail.com>
2019-12-16 13:09:18 +0200

    Initial Commit

---
+++ ScrollViewPaging/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ScrollViewPaging
+//
+//  Created by Ihor Pedan on 16.12.2019.
+//  Copyright © 2019 Ihor Pedan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
